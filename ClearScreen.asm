d244766bde04ac646a0b5a2d56934318881ef84f
HuyVuong <vuonghuy12345@gmail.com>
2023-09-03 11:43:21 +0700

    first commit

---
+++ ClearScreen.asm
@@ -0,0 +1,8 @@
+Clear proc
+    push	BX CX DX SI	;
+               mov ah, 00
+               mov al,02
+               int 10h
+               mov ah, 4bh
+               int 21h
+               ret
---
+++ VeDuongThang.asm
@@ -0,0 +1,67 @@
+.MODEL small
+.STACK 100h
+.DATA
+    msg  db 'Nhan 0 de thoat:$'
+
+.CODE
+    CONTAINER:  
+                mov ax, @data
+                mov ds, ax
+              
+    ; Draw
+    ; setup
+                mov ah,0          ; set video mode
+                mov al,13h        ; graphical mode. 40*25.256 colors.320*200 pixels. 1page
+                int 10h
+
+                mov ah,0ch
+                mov al, 10        ; set color of pixel
+                mov cx,30         ;row(x)
+                mov dx, 30        ; column(y)
+                int 10h
+
+    ;   Drawing
+    ; ngang
+    horizontal: 
+                int 10h
+                inc cx
+                cmp cx, 100
+                jbe horizontal
+    ; doc
+                mov cx, 130
+    vertical:   
+                int 10h
+                inc dx
+                cmp dx, 100
+                jbe vertical
+    ; chéo
+                mov cx, 160
+                mov dx, 30
+    cross:      
+                int 10h
+                inc cx
+                inc dx
+                cmp dx, 100
+                jbe cross
+               
+    messAndExit:
+                lea dx, msg
+                mov ah,09h
+                int 21h
+
+                mov ah,01h
+                int 21h
+                sub al,'0'
+                cmp al,0
+                mov ah, 00
+                mov al,02
+                int 10h
+                mov ah, 4bh
+                int 21h
+  
+
+    ;   Return to DOs
+              
+                mov ah, 4ch
+                int 21h
+end CONTAINER
